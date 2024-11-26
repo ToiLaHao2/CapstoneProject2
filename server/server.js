33622489b3a5275252ded70defa8879590dbe8fc
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2024-11-26 13:46:43 +0700

    update .env

--- server/server.js
+++ server/server.js
@@ -1,2 +1,4 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const dotenv = require("dotenv");
