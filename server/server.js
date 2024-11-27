e30ad99b99be720e7e78747abd3ad129b6c6d5cf
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2024-11-27 16:17:26 +0700

    update
    write some lines in file server.js

--- server/server.js
+++ server/server.js
@@ -3,4 +3,17 @@ const cors = require("cors");
 const bodyparser = require("body-parser");
 const dotenv = require("dotenv");
 
+dotenv.config();
 
+const app = express();
+const port = process.env.PORT;
+
+// Middlewares
+app.use(cors());
+app.use(bodyparser.json());
+
+// Connect database
+
+// Routes
+
+// Listen
