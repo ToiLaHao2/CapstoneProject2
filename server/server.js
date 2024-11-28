24a5c504086ef9d9429af3d0bbc261415ba0612f
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2024-11-28 11:18:25 +0700

    update and add new features
    - fix file dbConfig.js.
    - adding function in authController.js
       + Register
       + Login
       + Change password
    - test log files in logs folder
    - test run server and connect to mongoDb
    - Adding validation rules in validationRules.js
    - Adding validate logic in validate.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyparser = require("body-parser");
 const dotenv = require("dotenv");
 
 const errorHandler = require("./middleware/errorHandler");
+const { connectDb } = require("./configs/dbConfig");
 
 dotenv.config();
 
@@ -17,7 +18,10 @@ app.use(bodyparser.json());
 app.use(errorHandler);
 
 // Connect database
-
+connectDb();
 // Routes
 
 // Listen
+app.listen(port, function () {
+  console.log("Your app running on port " + port);
+});
