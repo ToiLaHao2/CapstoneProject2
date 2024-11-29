944484f7092e5277254a1a0403bc750a67bfd084
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2024-11-29 17:21:18 +0700

    update

---
+++ server/controllers/boardController.js
@@ -0,0 +1,6 @@
+const Board = require("../models/Board");
+const User = require("../models/User");
+
+async function CreateBoard(req, res) {
+    
+}
--- server/utils/validation/validationRules.js
+++ server/utils/validation/validationRules.js
@@ -1,41 +1,62 @@
 const validationRules = {
-    //Auth validate
-    register: {
-        checkMessage: "Register new account",
-        requiredFields: [
-            "user_full_name",
-            "user_email",
-            "user_password",
-            "user_avatar_url",
-            "checkMessage"
-        ],
-        minLength: {
-            user_name: 3,
-            user_password: 6
-        },
-        maxLength: {
-            user_phone: 10
-        },
-        regex: {
-            user_email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-        }
+  //Auth validate
+  register: {
+    checkMessage: "Register new account",
+    requiredFields: [
+      "user_full_name",
+      "user_email",
+      "user_password",
+      "user_avatar_url",
+      "checkMessage",
+    ],
+    minLength: {
+      user_name: 3,
+      user_password: 6,
     },
-    login: {
-        checkMessage: "Login to account",
-        requiredFields: ["user_email", "user_password", "checkMessage"]
+    maxLength: {
+      user_phone: 10,
     },
-    changePassword: {
-        checkMessage: "Change password",
-        requiredFields: [
-            "user_email",
-            "user_password",
-            "user_last_password",
-            "checkMessage"
-        ],
-        minLength: {
-            user_password: 6
-        }
-    }
+    regex: {
+      user_email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    },
+  },
+  login: {
+    checkMessage: "Login to account",
+    requiredFields: ["user_email", "user_password", "checkMessage"],
+  },
+  changePassword: {
+    checkMessage: "Change password",
+    requiredFields: [
+      "user_email",
+      "user_password",
+      "user_last_password",
+      "checkMessage",
+    ],
+    minLength: {
+      user_password: 6,
+    },
+  },
+  // Board validate
+  createBoard: {
+    checkMessage: "Create new board",
+    requiredFields: [
+      "board_title",
+      "board_description",
+      "board_is_public",
+      "board_collaborators",
+      "board_list",
+      "checkMessage",
+    ],
+    maxLength: {
+      board_description: 500,
+    },
+  },
+  updateBoard: {
+    checkMessage: "Update board",
+  },
+  deleteBoard: {
+    checkMessage: "Delete board",
+  },
 };
 
 module.exports = { validationRules };
