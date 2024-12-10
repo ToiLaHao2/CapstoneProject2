9c56bbdba216c04a3958d0ab71aa92cea56c0e31
toilahao2 <131520813+ToiLaHao2@users.noreply.github.com>
2024-12-10 22:06:31 +0700

    uipdatesffas

--- server/models/Card.js
+++ server/models/Card.js
@@ -19,6 +19,7 @@ const CardSchema = new Schema({
         {
             card_assignee_id: {
                 type: Schema.Types.ObjectId,
+                ref:"User",
                 required: true
             }
         }
@@ -27,17 +28,20 @@ const CardSchema = new Schema({
         {
             card_label_id: {
                 type: Schema.Types.ObjectId,
+                ref:"Label",
                 required: true
             }
         }
     ],
     card_checklist_id: {
-        type: Schema.Types.ObjectId
+        type: Schema.Types.ObjectId,
+        ref: "Checklist",
     },
     card_attachments: [
         {
             card_attachment_id: {
                 type: Schema.Types.ObjectId,
+                ref: "Attachment",
                 required: true
             }
         }
@@ -46,6 +50,7 @@ const CardSchema = new Schema({
         {
             card_comment_id: {
                 type: Schema.Types.ObjectId,
+                ref: "Comment",
                 required: true
             }
         }
--- server/utils/validation/validationRules.js
+++ server/utils/validation/validationRules.js
@@ -1,113 +1,118 @@
 const validationRules = {
-  //Auth middleware validate
-  register: {
-    checkMessage: "Register new account",
-    requiredFields: [
-      "user_full_name",
-      "user_email",
-      "user_password",
-      "user_avatar_url",
-      "checkMessage",
-    ],
-    minLength: {
-      user_name: 3,
-      user_password: 6,
+    //Auth middleware validate
+    register: {
+        checkMessage: "Register new account",
+        requiredFields: [
+            "user_full_name",
+            "user_email",
+            "user_password",
+            "user_avatar_url",
+            "checkMessage"
+        ],
+        minLength: {
+            user_name: 3,
+            user_password: 6
+        },
+        maxLength: {
+            user_phone: 10
+        },
+        regex: {
+            user_email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        }
     },
-    maxLength: {
-      user_phone: 10,
+    login: {
+        checkMessage: "Login to account",
+        requiredFields: ["user_email", "user_password", "checkMessage"]
     },
-    regex: {
-      user_email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    changePassword: {
+        checkMessage: "Change password",
+        requiredFields: [
+            "user_email",
+            "user_password",
+            "user_last_password",
+            "checkMessage"
+        ],
+        minLength: {
+            user_password: 6
+        }
     },
-  },
-  login: {
-    checkMessage: "Login to account",
-    requiredFields: ["user_email", "user_password", "checkMessage"],
-  },
-  changePassword: {
-    checkMessage: "Change password",
-    requiredFields: [
-      "user_email",
-      "user_password",
-      "user_last_password",
-      "checkMessage",
-    ],
-    minLength: {
-      user_password: 6,
+    // User middle validate
+    getUserProfile: {
+        checkMessage: "Get user profile",
+        requiredFields: ["user_id", "checkMessage"]
     },
-  },
-  // User middle validate
-  getUserProfile: {
-    checkMessage: "Get user profile",
-    requiredFields: ["user_id", "checkMessage"],
-  },
-  updateUser: {
-    checkMessage: "Update user info",
-  },
-  //   getUserProfile
-  // updateUser
-  // uploadProfilePicture
-  // getAllUserInBoard
-  // addUserToBoard
-  // removeUserFromBoard
-  // updateUserRoleInBoard
-  // assignUserToCard
-  // removeUserToCard
-  // getUserCards
-  // searchUsers
-  // suggestUsersToAdd
-  // updateNotificationsSettings
-  // getUserNotifications
-  // creatWorkGroup(đang xem xét)
+    updateUser: {
+        checkMessage: "Update user info"
+    },
+    // getUserProfile
+    // updateUser
+    // uploadProfilePicture
+    // getAllUserInBoard
+    // addUserToBoard
+    // removeUserFromBoard
+    // updateUserRoleInBoard
+    // assignUserToCard
+    // removeUserToCard
+    // getUserCards
+    // searchUsers
+    // suggestUsersToAdd
+    // updateNotificationsSettings
+    // getUserNotifications
+    // creatWorkGroup(đang xem xét)
 
-  inviteUserToBoardByEmail: {
-    checkMessage: "Invite user to board by email",
-    requiredFields: ["board_id", "user_email", "checkMessage"],
-  },
-  // Board middleware validate
-  createBoard: {
-    checkMessage: "Create new board",
-    requiredFields: [
-      "board_title",
-      "board_description",
-      "board_is_public",
-      "board_collaborators",
-      "board_list",
-      "checkMessage",
-    ],
-    maxLength: {
-      board_description: 500,
+    inviteUserToBoardByEmail: {
+        checkMessage: "Invite user to board by email",
+        requiredFields: ["board_id", "user_email", "checkMessage"]
     },
-  },
-  getBoard: {
-    checkMessage: "Get board",
-    requiredFields: ["board_id", "checkMessage"],
-  },
-  updateBoard: {
-    checkMessage: "Update board",
-    requiredFields: ["board_id", "board_update_details"],
-  },
-  deleteBoard: {
-    checkMessage: "Delete board",
-    requiredFields: ["board_id"],
-  },
-  //   createBoard
-  // getBoard
-  // updateBoard
-  // deleteBoard
-  // addMemberToBoard
-  // removeMemberFromBoard
-  // updateMemberRole
-  // getAllMembers
-  // updatePrivacy
-  // checkUserAccess
-  // getListsInBoard
-  // addListToBoard
-  // moveList
-  // getCardsInBoard
-  // archiveCard
-  // updateBoardSetting
-  // assignLabelsToBoard
+    // Board middleware validate
+    // createBoard
+    createBoard: {
+        checkMessage: "Create new board",
+        requiredFields: [
+            "board_title",
+            "board_description",
+            "board_is_public",
+            "board_collaborators",
+            "board_list",
+            "checkMessage"
+        ],
+        maxLength: {
+            board_description: 500
+        }
+    },
+    // getBoard
+    getBoard: {
+        checkMessage: "Get board",
+        requiredFields: ["board_id", "checkMessage"]
+    },
+    // getBoardsByUserId
+    getBoardsByUserId: {
+        checkMessage: "Get boards by user id",
+        requiredFields: ["user_id", "checkMessage"]
+    },
+    // updateBoard
+    updateBoard: {
+        checkMessage: "Update board",
+        requiredFields: ["board_id", "board_update_details"]
+    },
+    // deleteBoard
+    deleteBoard: {
+        checkMessage: "Delete board",
+        requiredFields: ["board_id"]
+    }
+    // addMemberToBoard
+    // removeMemberFromBoard
+    // updateMemberRole
+    // getAllMembers
+    // updatePrivacy
+    // checkUserAccess
+    // getListsInBoard
+    // addListToBoard
+    // moveList
+    // getCardsInBoard
+    // archiveCard
+    // updateBoardSetting
+    // assignLabelsToBoard
 };
 
 module.exports = { validationRules };
