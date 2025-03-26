835a9a8d3fe1ed09e82bec9db541584544e8894e
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2025-03-26 14:44:07 +0700

    myapi

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Cấu hình cơ bản
-const BASE_URL = "http://localhost:5001/api";
+const BASE_URL = "http://localhost:5000/api";
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
