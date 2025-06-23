da762fd95dda47a5148ac7fb3d439b26fafaf79f
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2025-06-23 21:44:37 +0700

    update

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // Cấu hình cơ bản
-const BASE_URL = "http://localhost:5000/api";
-// const BASE_URL = "http://mywebcapstone.myddns.me:5000/api";
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "http://tasktaskminderstudio.3utilities.com:5000/api";
 // const BASE_URL = "http://172.16.10.154:5000/api";
 
 export const axiosInstance = axios.create({
