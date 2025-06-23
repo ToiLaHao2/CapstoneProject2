558a8403526d08560089a66b73095684a86e8a7c
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2025-06-23 23:08:35 +0700

    update

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -4,6 +4,7 @@ import axios from "axios";
 // const BASE_URL = "http://localhost:5000/api";
 const BASE_URL = "http://tasktaskminderstudio.3utilities.com:5000/api";
 // const BASE_URL = "http://172.16.10.154:5000/api";
+// const BASE_URL = "https://taskminderstudio.onrender.com";
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
