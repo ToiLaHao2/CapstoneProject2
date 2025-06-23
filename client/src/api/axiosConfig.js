f7613a8be342966764f0e2b5d69ecc9d3508d4b9
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2025-06-23 23:56:02 +0700

    Update axiosConfig.js

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // const BASE_URL = "http://localhost:5000/api";
 // const BASE_URL = "http://taskminderstudio.3utilities.com:5000/api";
 // const BASE_URL = "http://172.16.10.154:5000/api";
-const BASE_URL = "https://capstoneproject2-9mv8.onrender.com";
+const BASE_URL = "https://capstoneproject2-9mv8.onrender.com/api";
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
