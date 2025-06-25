9f1c9a452d13e26ed45a44cfea7c8092a13dff96
maidayne <mai.nguyenthi.cit19@eiu.edu.vn>
2025-06-25 16:55:38 +0700

    up css

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 // Cấu hình cơ bản
-// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "http://localhost:5000/api";
 // const BASE_URL = "http://taskminderstudio.3utilities.com:5000/api";
 // const BASE_URL = "http://172.16.10.154:5000/api";
-const BASE_URL = "https://capstoneproject2-9mv8.onrender.com/api";
+// const BASE_URL = "https://capstoneproject2-9mv8.onrender.com/api";
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
